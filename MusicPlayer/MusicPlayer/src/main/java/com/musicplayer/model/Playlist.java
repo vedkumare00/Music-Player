0854fef52ed53f97501d61d503e5a0a6a6ed71a1
Ved <ved.2405166103@vcet.edu.in>
2026-04-10 10:26:23 +0530

    Applied gitignore to remove large files

--- MusicPlayer/MusicPlayer/src/main/java/com/musicplayer/model/Playlist.java
+++ MusicPlayer/MusicPlayer/src/main/java/com/musicplayer/model/Playlist.java
@@ -1,8 +1,17 @@
 package com.musicplayer.model;
 
-import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "playlists")
